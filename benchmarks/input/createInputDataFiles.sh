06c12bf29ebd7e77bc623ed97e5f87c73492ad67
Roland Kaufmann <roland.kaufmann@uni.no>
2013-02-08 22:07:10 +0100

    Have the compiler embed data files instead of strings
    This solution is not portable as it is locked to the GNU toolchain, but
    then again using long strings is not portable either because not all
    compilers support that.

--- benchmarks/input/createInputDataFiles.sh
+++ benchmarks/input/createInputDataFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script file is used to generate input files used by upscale_relperm_benchmark. 
-# Three files must be specified below:
+# Three files must be specified on the command line::
 #   - An eclipse grid file
 #   - A stone file 
 #   - An output/result file from running upscale_relperm on a equal problem
@@ -11,79 +11,70 @@
 #         - upscales only one phase
 #       Remember that other options also must match (e.g., points, relPermCurve, jFunctionCurve, ...)
 #
-# These files are combined into a C++ syntax file for declaring the data as strings. 
-# The resulting file must be included in upscale_relperm_benchmark.
+# If a fourth argument is given, this is used as the relative path to this
+# directory from the compilation unit where these files are included.
+#
+# The output from this program is C++ syntax for a program declaring the data as
+# string. This output can be piped into a file which must be included in
+# upscale_relperm_benchmark.
+#
+# Example:
+#   ./createInputDataFiles.sh bench20.grdecl stone.txt benchmark.txt input/ > bench20.cpp
 
-# Define files as variables
-gridfile=benchmark75.grdecl
-resultfile=benchmark75_upscaled_relperm_20points.txt
-stonefile=stonefile_benchmark.txt
-cfile=benchmark75_input_data.cpp
+# check for correct number of arguments
+if [ $# -lt 3 -o $# -gt 4 ]; then
+    echo Usage: $0 gridfile stonefile resultfile [reldir] 1>&2
+    exit 1
+fi
 
-tmp1=temp_file1.txt
-tmp2=temp_file2.txt
-tmp3=temp_file3.txt
+# names for each of the inputs that are specified
+gridfile="$1"
+stonefile="$2"
+resultfile="$3"
+reldir="$4"
 
-################################
-# Eclipse file 
-################################
+# header which contains timestamp and description
+cat <<EOF
+// Date: $(date +"%F %T %Z")
+// This file is created from createInputDataFiles.sh.
+// It contains input data and reference solution stored as strings.
+// To be included in upscale_relperm_benchmark.
 
-# Insert quotes and newline character on every line, and insert ; on the last line. Store in $tmp1
-sed -e 's/.*/"&\\n"/g' -e '$s/\\n"/";/' <$gridfile >$tmp1
+EOF
 
-# Insert declaration of string on first line
-sed -i '1istring eclipseInput =' $tmp1
+# filenames embedded to recreate original files
+cat <<EOF
+static const char* ECLIPSEFILENAME = "$gridfile";
+static const char* ROCKFILENAME = "$stonefile";
+static const char* REFERENCEFILENAME = "$resultfile";
 
-# Add extra line
-echo "" >> $tmp1
+EOF
 
-################################
-# Result file
-################################
+# macro to embed files; first include a file as a local
+# label (plus a zero terminator, so it can be read as string),
+# then a symbol which is a pointer to this label; align
+# to 64 bits to satisfy various architectures
+cat <<EOF
+#define EXTERN_FILE(type,name,file) \\
+  __asm__ (               \\
+    ".global " #name "\n" \\
+    ".data\n"             \\
+    ".p2align 3\n"        \\
+    "1:\n"                \\
+    ".incbin " #file "\n" \\
+    ".byte 0, 0\n"        \\
+    ".p2align 3\n"        \\
+    #name ":\n"           \\
+    ".int 1b\n"           \\
+    );                    \\
+  extern type *name
 
-# Delete comments, insert quotes and newline character on every line, and insert ; on last line. Store in $tmp2
-sed -e '/#.*/ d' -e 's/.*/"&\\n"/g' -e '$s/\\n"/";/' <$resultfile >$tmp2
+EOF
 
-# Insert declaration of string on first line
-sed -i '1istring result_string =' $tmp2
-
-# Add extra line
-echo "" >> $tmp2
-
-################################
-# Stone file
-################################
-
-# Delete comments, insert quotes and newline character on every line, and insert ; on last line. Store in $tmp3
-sed -e '/#.*/ d' -e 's/.*/"&\\n"/g' -e '$s/\\n"/";/' <$stonefile >$tmp3
-
-# Insert declaration of string on first line
-sed -i '1istring stone_string =' $tmp3
-
-# Add extra line
-echo "" >> $tmp3
-
-################################
-# Merge files
-################################
-
-echo "// Date: "$(date) > $cfile 
-echo "// This file is created from createInputDataFiles.sh." >> $cfile
-echo "// It contains input data and reference solution stored as strings." >> $cfile
-echo "// To be included in upscale_relperm_benchmark." >> $cfile
-echo "" >> $cfile
-echo "using namespace std;" >> $cfile
-echo "" >> $cfile
-
-# Add file name variables as strings
-echo "static char* ECLIPSEFILENAME = \""$gridfile"\";" >> $cfile
-echo "static char* ROCKFILENAME = \""$stonefile"\";" >> $cfile
-echo "static char* REFERENCEFILENAME = \""$resultfile"\";" >> $cfile
-echo "" >> $cfile
-
-cat $tmp1 $tmp2 $tmp3 >> $cfile
-
-# Finally, remove temp files
-rm $tmp1
-rm $tmp2
-rm $tmp3
+# embed the files we need (this is not done when running the
+# script but rather when compiling the resulting output!)
+cat <<EOF
+EXTERN_FILE(const char,eclipseInput,"$reldir$gridfile");
+EXTERN_FILE(const char,stone_string,"$reldir$stonefile");
+EXTERN_FILE(const char,result_string,"$reldir$resultfile");
+EOF
