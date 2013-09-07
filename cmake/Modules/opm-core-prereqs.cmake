72ea1b835df5de5aa874ca2724f98243fb3340ca
Roland Kaufmann <roland.kaufmann@uni.no>
2013-09-07 23:11:32 +0200

    Depend on Boost::iostreams
    The benchmark library uses Boost::iostreams to do decompression. Since
    we only scan for the Boost dependency once, this submodule is added to
    all of the projects in order to have a coherent dependency on Boost.

--- cmake/Modules/opm-core-prereqs.cmake
+++ cmake/Modules/opm-core-prereqs.cmake
@@ -15,7 +15,7 @@ set (opm-core_DEPS
 	"CXX11Features REQUIRED"
 	# various runtime library enhancements
 	"Boost 1.39.0
-		COMPONENTS date_time filesystem system unit_test_framework REQUIRED"
+		COMPONENTS date_time filesystem system iostreams unit_test_framework REQUIRED"
 	# matrix library
 	"BLAS REQUIRED"
 	"LAPACK REQUIRED"
